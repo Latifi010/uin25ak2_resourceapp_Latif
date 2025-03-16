fae3c54a392551904fa8557f9f30b458165494c7
Latifi010 <amaklatif@gmail.com>
2025-03-16 23:15:00 +0100

    Siste push
    final

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -1,9 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'; 
-import App from './App.jsx'
+import { StrictMode } from 'react' // Importerer StrictMode fra react
+import { createRoot } from 'react-dom/client' // Importerer createRoot fra react-dom/client
+import { BrowserRouter } from 'react-router-dom'; // Importerer BrowserRouter fra react-router-dom
+import App from './App.jsx' // Importerer App-komponenten fra App.jsx
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')).render( // Lager en rot-node og rendrer App-komponenten i rot-noden, dette er en nyere måte å rendere komponenter på. (ifl YT)
   <StrictMode>
     <BrowserRouter> {/* Prøver å wrapppe opp app, test 1 */}
       <App />
